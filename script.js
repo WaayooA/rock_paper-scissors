d2dbb30082f56a9c293126d7f9e7b86c5cdb3c66
WaayooA <153339351+WaayooA@users.noreply.github.com>
2024-01-30 21:42:50 -0600

    Add principal functions to the JS file

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
 
 // Make function to make the computer choose randomly
-//
+// Make a function that play a round between the player and the computer
+// 
+
+function getComputerChoice() {
+
+    const min = 1;
+    const max = 3;
+    let randomNumber = 0;
+    randomNumber = Math.floor(min + (max-min+1)*Math.random()); 
+
+    if(randomNumber === 1){
+        return "rock";
+    }
+    else if(randomNumber === 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+function getPlayerSelection(){
+
+    let input = "";
+    input = prompt("Write rock, paper or scissors");
+    input = input.toLowerCase().trim();
+
+    if(input === "rock" || input === "paper" || input === "scissors"){
+    
+        return input;
+
+    }
+    else {
+        console.log("You type incorrectly");
+    }
+}
+
+function playRound(playerSelection,computerSelection){
+
+    if(playerSelection === computerSelection){
+        console.log("player: " + playerSelection + "  VS  " + "Computer: " + computerSelection + "    This is a tie");
+    }
+    else if(playerSelection === "rock"){
+        
+    }
+
+}
+
+playRound(getPlayerSelection(),getComputerChoice());
