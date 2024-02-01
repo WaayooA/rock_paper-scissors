93fbf0a5333b0a04d82e00e63a6a577ec4b11f1c
WaayooA <153339351+WaayooA@users.noreply.github.com>
2024-01-31 20:45:37 -0600

    finish functions and the game playable

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock_paper-scissors
+
+In this project we make a game rock, paper, scissors in a webpage thruogh the console
--- script.js
+++ script.js
@@ -39,13 +39,73 @@ function getPlayerSelection(){
 
 function playRound(playerSelection,computerSelection){
 
+    //if we tie the round return 0
+
     if(playerSelection === computerSelection){
         console.log("player: " + playerSelection + "  VS  " + "Computer: " + computerSelection + "    This is a tie");
+        return 0;
     }
-    else if(playerSelection === "rock"){
+
+    //if player wins return 1
+
+    else if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
         
+        console.log("player: " + playerSelection + "  VS  " + "Computer: " + computerSelection + "    You win");
+        return 1;
+
+    }
+
+    //If computer wins return 2
+
+    else{
+
+        console.log("player: " + playerSelection + "  VS  " + "Computer: " + computerSelection + "    Computer wins");
+        return 2;
+
     }
 
 }
 
-playRound(getPlayerSelection(),getComputerChoice());
+// This function will control de score and every round, well play the best of 5
+
+function playGame(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+    let trackWinner = 0;
+
+    console.log("Rock,Paper,Scissors the best of 5")
+
+    for(let i = 0; i < 5; i++){
+        console.log("Player Score: " + playerScore + "           Computer Score: " + computerScore);
+        trackWinner =  playRound(getPlayerSelection(),getComputerChoice());
+
+        if(trackWinner === 0){
+            --i;
+        }
+        else if(trackWinner === 1){
+            ++playerScore;
+        }
+        else{
+            ++computerScore;
+        }
+
+    }
+
+    console.log("Player Score: " + playerScore + "           Computer Score: " + computerScore);
+    
+    if(playerScore > computerScore){
+        
+        console.log("You won!");
+
+    }
+    else{
+
+        console.log("Computer wins");
+
+    }
+
+}
+
+
+playGame();
